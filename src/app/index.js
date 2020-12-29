b7b71bda5663330268b4f34a89672d3efd3e4aa5
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-29 01:36:58 +0000

    imported the files to index.js

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,18 @@
+import { addToHourDropdown, addToMinuteDropdown } from "./addToDropdown";
+import { alarmSet } from "./alarmSet";
+import { disableAlarm } from "./disableAlarm";
+import { playAlarm } from "./playAlarm";
+import { updateTime } from "./updateTime";
+
 import "@styles/main.scss";
+
+const App = () => {
+  addToHourDropdown();
+  addToMinuteDropdown();
+  alarmSet();
+  setInterval(updateTime, 1000);
+  setInterval(playAlarm, 1000);
+  disableAlarm();
+};
+
+App();
